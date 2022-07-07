e413554fae3fc2384f520ebf6eb97df0e706f30d
latinyanin <latinyanin@gmail.com>
2022-07-07 19:58:41 +0000

    fix: smtp settings

--- .devcontainer/develop.settings.php
+++ .devcontainer/develop.settings.php
@@ -20,14 +20,17 @@ $databases = [
 ];
 
 $settings["file_temp_path"] = '/tmp';
-$settings['config_sync_directory'] = 'configs';
+$settings['config_sync_directory'] = dirname(DRUPAL_ROOT) . '/configs';
 $settings['file_public_path'] = 'sites/default/files';
 $settings['file_private_path'] = __DIR__ . '/files/private';
 $settings['trusted_host_patterns'] = ['^localhost$'];
 $settings["hash_salt"] = 'sgaftdghyrtwhybfdazfdsagtrehyrth';
-$conf['smtp_on'] = TRUE;
-$conf['smtp_host'] = 'mailhog';
-$conf['smtp_port'] = '1025';
-$conf['smtp_username'] = '';
-$conf['smtp_password'] = '';
-$conf['smtp_allowhtml'] = TRUE;
+$config['smtp.settings']['smtp_on'] = TRUE;
+$config['smtp.settings']['smtp_host'] = 'mailhog';
+$config['smtp.settings']['smtp_port'] = '1025';
+$config['smtp.settings']['smtp_username'] = '';
+$config['smtp.settings']['smtp_password'] = '';
+$config['smtp.settings']['smtp_protocol'] = 'standard';
+$config['smtp.settings']['smtp_autotls'] = false;
+$config['smtp.settings']['smtp_timeout'] = 30;
+$config['smtp.settings']['smtp_allowhtml'] = 1;
