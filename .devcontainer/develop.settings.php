1b335d7420fac05475ff5ef954d1039b967a00f6
latinyanin <latinyanin@gmail.com>
2022-07-07 14:07:02 +0000

    feat: add settings.php

---
+++ .devcontainer/develop.settings.php
@@ -0,0 +1,34 @@
+
+<?php
+
+/**
+ * @file
+ * Drupal site-specific configuration file.
+ */
+
+$databases = [
+  "default" => [
+    "default" => [
+      "database" => $_ENV['MYSQL_DATABASE'],
+      "username" => $_ENV['MYSQL_USER'],
+      "password" => $_ENV['MYSQL_PASSWORD'],
+      "host" => "db",
+      "port" => "3306",
+      "driver" => "mysql",
+      "prefix" => "",
+    ],
+  ],
+];
+
+$settings["file_temp_path"] = '/tmp';
+$settings['config_sync_directory'] = 'configs';
+$settings['file_public_path'] = 'sites/default/files';
+$settings['file_private_path'] = __DIR__ . '/files/private';
+$settings['trusted_host_patterns'] = ['^localhost$'];
+$settings["hash_salt"] = 'sgaftdghyrtwhybfdazfdsagtrehyrth';
+$conf['smtp_on'] = TRUE;
+$conf['smtp_host'] = 'mailhog';
+$conf['smtp_port'] = '1025';
+$conf['smtp_username'] = '';
+$conf['smtp_password'] = '';
+$conf['smtp_allowhtml'] = TRUE;
